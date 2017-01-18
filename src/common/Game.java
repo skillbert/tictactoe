12e6f7851e45d2105d5665126694c157f1518e3d
Maurits van der Vijgh <mauritsvdvijgh@gmail.com>
2017-01-18 20:01:36 +0100

    Added most of the tictactoe code

--- src/common/Game.java
+++ src/common/Game.java
@@ -1,9 +1,49 @@
 package common;
 
 public class Game {
+    public static final int NUMBER_PLAYERS = 2;
 	private Board board;
+	private Player[] players;
+	private int turn;
 	
-	public Game(){
+	
+	public Game(Player p1, Player p2){
 		board=new Board();
+		players = new Player[NUMBER_PLAYERS];
+		players[0] = p1;
+		players[1] = p2;
+		turn = 0;
 	}
+	
+	public void start() {
+	    boolean game_end = false;
+	    while (!game_end) {
+	        reset();
+	        play();
+	    }
+	}
+	    
+	public void reset() {
+	    turn = 0;
+	    board.reset();
+	}
+	
+	public void play() {
+	    while(!board.gameOver()) {
+	        update();
+	        Player player = players[turn%NUMBER_PLAYERS];
+	        board.setField(player.determineMove(board), player.getMark());
+	        turn += 1;
+	    }
+	    if(board.isWinner(players[(turn-1)%NUMBER_PLAYERS].getMark())) {
+	        System.out.println(players[(turn-1)%NUMBER_PLAYERS].getName() + " Wins");
+	    } else {
+	        System.out.println("Board full, no winners.");
+	    }
+	}
+	
+	private void update() {
+	    System.out.println(board.toString());
+	}
+	
 }
--- src/common/Player.java
+++ src/common/Player.java
@@ -1,5 +1,27 @@
 package common;
 
-public interface Player {
-
+public abstract class Player {
+    private String name;
+    private Mark mark;
+    private PlayerState state;
+    
+    public Player(String name, Mark mark) {
+        this.name = name;
+        this.mark = mark;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public Mark getMark() {
+        return mark;
+    }
+    
+    public abstract int determineMove(Board board);
+    
+    public void makeMove(Board board) {
+        int choice = determineMove(board);
+        board.setField(choice, getMark());
+    }
 }
